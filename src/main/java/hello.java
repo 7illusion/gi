3d08a470b5a8a0f52bfa392868838c8d9ebf849e
7illusion <1559647@gmail.com>
2022-12-26 16:30:26 +0900

    first commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("안녕");
+        System.out.println("안녕하세요");
     }
 }
