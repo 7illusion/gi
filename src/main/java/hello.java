e19741948d181031f4d1963773418a67556d60cc
7illusion <1559647@gmail.com>
2022-12-26 19:35:25 +0900

    first commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("안녕하세요");
+        System.out.println("시작은 순서대로 그림을 그려서 나열을 해보기 ");
     }
 }
